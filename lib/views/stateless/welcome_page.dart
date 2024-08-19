358197e3e8e71469587dddbdafc65438600b5824
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-08-20 00:36:34 +0200

    Layout has been fixed on welcome screen.

--- lib/views/stateless/welcome_page.dart
+++ lib/views/stateless/welcome_page.dart
@@ -2,22 +2,80 @@ import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Welcome", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.blue,
-      ),
-      body: const Center(
+      backgroundColor: Colors.blue[800],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return _welcomeView(context, constraints.maxWidth, constraints.maxHeight);
+        }
+      )
+    );
+  }
+
+  Widget _welcomeView(BuildContext context, double screenWidth, double screenHeight) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Hello World!"),
+          children: <Widget>[
+            Text(
+              "Welcome to TeamVortex!\nWe're glad to have you here!",
+              style: TextStyle(
+                // First value: fontSize reset limit.
+                // second value: fontSize value for exceeding limit.
+                // third value: fontSize value.
+                fontSize:
+                MediaQuery.of(context).size.width > 840 ? 42
+                :
+                MediaQuery.of(context).size.width * 0.05,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+                shadows: const [
+                  Shadow(
+                    blurRadius: 25.0,
+                    color: Colors.black,
+                    offset: Offset(5, 5),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: screenHeight > 650 ? 400 : screenHeight > 480 ? 200 : 100), //400 200 100
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.cyan[100]),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+              child: const Text("Login",
+                  style: TextStyle(fontSize: 20, color: Colors.black)),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.cyan[100]),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                ),
+              ),
+              child: const Text("Get Started",
+                  style: TextStyle(fontSize: 20, color: Colors.black)),
+            )
           ],
         ),
       ),
     );
   }
+
 }
