1ed76adbf483663383423cc0016f2a80c36da671
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-09-16 10:57:39 +0200

    A few comments have been added.

--- lib/models/entities/Account.dart
+++ lib/models/entities/Account.dart
@@ -2,16 +2,19 @@
 import 'dart:ffi';
 
 class Account {
+  //Attributes
   Long _id = 0 as Long;
   String _username = "";
   String _email = "";
   String _password = "";
 
+  //Properties
   Long get id => _id;
   String get username => _username;
   String get email => _email;
   String get password => _password;
 
+  //Constructor
   Account(Long id, String username, String email, String password) {
     _id = id;
     _username = username;
--- lib/viewmodels/login_register_vm.dart
+++ lib/viewmodels/login_register_vm.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:teamvortex/models/entities/Account.dart';
 
 class LoginRegisterViewModel extends ChangeNotifier {
+  late Account account;
   String username = "";
   String password = "";
   String email = "";
   String confirmPassword = "";
   String errorString = "";
+
+  // This function will check if the account information is correct.
+  // If it returns true then it proceeds to register the account into Firebase.
+  bool checkInfo() {
+    return false;
+  }
+
+  // It will be called into checkInfo() once the information has been validated to call Firebase and register the account information.
+  void _registerAccountIntoFirebase() {}
 }
